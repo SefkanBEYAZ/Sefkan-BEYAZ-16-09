b8e2b0a4c885f9a9f6526674ede14e18d9d36cd4
Kaanar <sefkanbeyaz@gmail.com>
2021-09-16 14:46:24 +0200

    Création de la classe Dictionary et Dictionary Test

---
+++ src/main/java/Sefkan/BEYAZ/TD/Dictionary.java
@@ -0,0 +1,12 @@
+package Sefkan.BEYAZ.TD;
+
+public class Dictionary {
+    
+    private String name;
+
+    public Dictionary(){}
+
+    public String getName(){
+        return "Example";
+    }
+}
---
+++ src/test/java/Sefkan/BEYAZ/TD/DictionaryTest.java
@@ -0,0 +1,23 @@
+package Sefkan.BEYAZ.TD;
+
+import org.junit.Before;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class DictionaryTest {
+    
+    private Dictionary dict;
+    
+    @Before
+    public void initialize(){
+        dict= new Dictionary();
+    }
+    @Test public void TestDictionaryName(){
+        
+        assertThat(dict.getName(),equalTo("Example"));
+
+    }
+}
